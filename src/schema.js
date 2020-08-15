ca75fd834717825688362fb210058db52a24cf38
Lee, Jung Geun <cjred77@gmail.com>
2020-08-16 08:27:32 +0900

    elasticsearch

--- src/schema.js
+++ src/schema.js
@@ -1,12 +1,17 @@
-import path from 'path'
-import {makeExecutableSchema, loadFilesSync, mergeTypeDefs, mergeResolvers} from 'graphql-tools'
+import path from "path";
+import {
+  makeExecutableSchema,
+  loadFilesSync,
+  mergeTypeDefs,
+  mergeResolvers,
+} from "graphql-tools";
 
-const allTypes = loadFilesSync(path.join(__dirname, '/api/**/*.graphql'))
-const allResolvers = loadFilesSync(path.join(__dirname, '/api/**/*.js'))
+const allTypes = loadFilesSync(path.join(__dirname, "/api/v2/**/*.graphql"));
+const allResolvers = loadFilesSync(path.join(__dirname, "/api/v2/**/*.js"));
 
 const schema = makeExecutableSchema({
   typeDefs: mergeTypeDefs(allTypes),
-  resolvers: mergeResolvers(allResolvers)
+  resolvers: mergeResolvers(allResolvers),
 });
 
 export default schema;
